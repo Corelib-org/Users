dcd73c868fd227b1be1eb002071c6613d48a7ee0
Steffen Sørensen <steffen@sublife.dk>
2008-08-15 11:28:03 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Users/tags/3_1_0@4744 516bd117-9ffd-0310-910c-af5d773169db

--- 3_0_0/Share/Database/MySQL/Upgrade/tbl_usersw.2671-2672.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `tbl_users` ADD `deleted` ENUM( 'TRUE', 'FALSE' ) NOT NULL DEFAULT 'FALSE' AFTER `activation_string` ;
-ALTER TABLE `tbl_users` ADD INDEX ( `deleted` ) ;
-ALTER TABLE `tbl_users`  COMMENT = 'Revision: 2672';
--- 3_0_0/Share/Database/MySQL/tbl_users_permssions.1.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `tbl_users_permissions` (
-  `pk_users_permissions` int(10) unsigned NOT NULL auto_increment,
-  `permission_ident` varchar(255) collate utf8_bin NOT NULL default '',
-  `permission_name` varchar(255) collate utf8_bin default NULL,
-  PRIMARY KEY  (`pk_users_permissions`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Revision: 1';
