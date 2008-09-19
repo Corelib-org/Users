6070f15f71b2dc6908200c37668ad41af15f6241
Steffen Sørensen <steffen@sublife.dk>
2008-09-19 22:13:00 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Users/trunk@4879 516bd117-9ffd-0310-910c-af5d773169db

--- Share/Database/MySQL/Upgrade/tbl_users_has_information.1-2.sql
+++ Share/Database/MySQL/Upgrade/tbl_users_has_information.1-2.sql
@@ -1,5 +1,3 @@
-ALTER TABLE `tbl_users_has_information` CHANGE `fk_information` `fk_information` SMALLINT( 6 ) UNSIGNED NOT NULL  
-ALTER TABLE `tbl_users_has_information` ADD FOREIGN KEY ( `fk_information_items` ) REFERENCES `getaling_dk_new`.`tbl_information_items` (
-`pk_information_items`
-) ON DELETE CASCADE ON UPDATE CASCADE ;
- ALTER TABLE `tbl_users_has_information`  COMMENT = 'Revision: 2' 
+ALTER TABLE `tbl_users_has_information` CHANGE `fk_information` `fk_information` SMALLINT( 6 ) UNSIGNED NOT NULL;  
+ALTER TABLE `tbl_users_has_information` ADD FOREIGN KEY ( `fk_information_items` ) REFERENCES `tbl_information_items` (`pk_information_items`) ON DELETE CASCADE ON UPDATE CASCADE ;
+ALTER TABLE `tbl_users_has_information`  COMMENT = 'Revision: 2' ;
