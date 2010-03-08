353a32622f753a91094b2b93702a02ba4286b15f
Steffen Sørensen <steffen@sublife.dk>
2010-03-08 14:31:34 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Users/trunk@5191 516bd117-9ffd-0310-910c-af5d773169db

--- Share/Database/MySQL/tbl_user_permissions.1.sql
+++ Share/Database/MySQL/tbl_user_permissions.1.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS `tbl_user_permissions` (
   `title` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `description` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`pk_user_permissions`),
-  UNIQUE KEY `permission_ident` (`ident`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Revision: 1' AUTO_INCREMENT=1 ;
+  UNIQUE KEY `ident` (`ident`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Revision: 1' AUTO_INCREMENT=1;
--- Share/Database/MySQL/tbl_users_has_permissions.1.sql
+++ Share/Database/MySQL/tbl_users_has_permissions.1.sql
@@ -7,3 +7,7 @@ CREATE TABLE IF NOT EXISTS `tbl_users_has_permissions` (
   KEY `expire` (`expire_timestamp`),
   KEY `fk_users_permissions` (`fk_user_permissions`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Revision: 1';
+
+ALTER TABLE `tbl_users_has_permissions`
+  ADD CONSTRAINT `tbl_users_has_permissions_ibfk_2` FOREIGN KEY (`fk_user_permissions`) REFERENCES `tbl_user_permissions` (`pk_user_permissions`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_users_has_permissions_ibfk_1` FOREIGN KEY (`fk_users`) REFERENCES `tbl_users` (`pk_users`) ON DELETE CASCADE ON UPDATE CASCADE;
